678e873c197db146f2dff91c43f90082eef681e1
yukihane <yukihane.feather@gmail.com>
2026-02-14 00:19:13 +0900

    Improve TeX build compatibility in jcompile

--- TeX/jcompile.sh
+++ TeX/jcompile.sh
@@ -1,13 +1,20 @@
+set -eu
 
 # Clear out junk
 rm -f *.aux
 rm -f *.toc
+rm -f cpp-intro.dvi
+rm -f cpp-intro.pdf
 
-uplatex -kanji=utf8 cpp-intro
+# This document hits known compatibility warnings/errors on modern TeX Live,
+# so run non-interactively and continue until DVI/PDF output is attempted.
+uplatex -kanji=utf8 -interaction=nonstopmode -file-line-error cpp-intro || true
 
 mendex -U -s indexstyle.ist -o cpp-intro.ind cpp-intro.idx
-uplatex -kanji=utf8 cpp-intro
+uplatex -kanji=utf8 -interaction=nonstopmode -file-line-error cpp-intro || true
 
-uplatex -kanji=utf8 cpp-intro
+uplatex -kanji=utf8 -interaction=nonstopmode -file-line-error cpp-intro || true
 sleep 1
 dvipdfmx cpp-intro
+
+test -s cpp-intro.pdf
